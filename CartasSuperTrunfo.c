a2183be41ab217e9d61380caea7e4ab9c8b25eec
Leo <108838370+leoruffini1@users.noreply.github.com>
2025-02-14 00:48:18 +0000

    FEITO CADASTRO DE CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,33 @@
 //Teste larissa
 
 int main() {
+
+    char codCidade[20];
+    char nome[20];
+    float area;
+    float pib;
+    int turismo;
+
+    printf("CADASTRO DE CIDADES PARA O SUPER TRUNFO\n CADASTRE CIDADES DE SÃO PAULO\n\n");
+
+        printf("Digite o código da cidade\n");
+        fgets(codCidade, 20, stdin);
+        
+        printf("Digite o nome da cidade da cidade\n");
+        fgets(nome, 20, stdin);
+
+        printf("Digite a área total da cidade em km quadrados\n");
+        scanf("%f", &area);
+
+        printf("Digite o PIB da cidade\n");
+        scanf("%f", &pib);
+       
+        printf("Digite quantos pontos turisticos a cidade possui\n");
+        scanf("%d", &turismo);
+       
+        printf("Código da cidade: %sNome da cidade: %sÁrea: %f \nPIB: %f \nNumero de pontos turistico: %d\n", codCidade, nome, area, pib, turismo);
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
