7049142b2c7f518a5528b6c665ea365d0b7677f4
Leo <108838370+leoruffini1@users.noreply.github.com>
2025-02-18 01:32:59 +0000

    FEITO REGISTRO DE CARTAS + IF ELSE

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,11 +8,11 @@
 
 int main() {
 
-    char codCidade[20];
-    char nome[20];
-    float area;
-    float pib;
-    int turismo;
+    char codCidade[20], codCidade2[20];
+    char nome[20], nome2[20];
+    float area, area2;
+    float pib, pib2;
+    int populacao, populacao2;
 
     printf("CADASTRO DE CIDADES PARA O SUPER TRUNFO\n CADASTRE CIDADES DE SÃO PAULO\n\n");
 
@@ -28,10 +28,62 @@ int main() {
         printf("Digite o PIB da cidade\n");
         scanf("%f", &pib);
        
-        printf("Digite quantos pontos turisticos a cidade possui\n");
-        scanf("%d", &turismo);
+        printf("Digite a população da cidade\n\n");
+        scanf("%d", &populacao);
        
-        printf("Código da cidade: %sNome da cidade: %sÁrea: %f \nPIB: %f \nNumero de pontos turistico: %d\n", codCidade, nome, area, pib, turismo);
+        printf("Carta registrada: %s \n\n", nome);
+        
+    //registro da segunda carta 
+
+    printf("CADASTRO DA SEGUNDA CARTA\n CADASTRE CIDADES DE SÃO PAULO\n\n");
+
+        printf("Digite o código da cidade\n");
+        fgets(codCidade2, 20, stdin);
+        
+        printf("Digite o nome da cidade da cidade\n");
+        fgets(nome2, 20, stdin);
+
+        printf("Digite a área total da cidade em km quadrados\n");
+        scanf("%f", &area2);
+
+        printf("Digite o PIB da cidade\n");
+        scanf("%f", &pib2);
+       
+        printf("Digite a população da cidade\n");
+        scanf("%d", &populacao2);
+       
+        printf("Carta registrada: %s \n\n", nome2); 
+
+
+        //calculo da primera carta
+        float densidade = populacao/area;
+        float pibCapta = pib/populacao;
+
+        //calculo da segunda carta
+        float densidade2 = populacao2/area2;
+        float pibCapta2 = pib2/populacao2 ;
+
+        printf("Carta: %sPIB per capta: %0.2f - Densidade Populacional: %0.2f\n\n", nome, pibCapta, densidade);
+        printf("Carta: %sPIB per capta: %0.2f - Densidade Populacional: %0.2f\n\n", nome2, pibCapta2, densidade2);
+
+        if(pibCapta > pibCapta2){
+            printf("A carta %s tem o PIB per capta maior\n", nome);
+        }
+        else{
+            printf("A carta %s tem o PIB per capta maior\n", nome2);
+        }
+
+        if(densidade > densidade2){
+            printf("A carta %s tem a Densidade Populacional maior\n", nome);
+        }
+        else{
+            printf("A carta %s tem a Densidade Populacional maior\n", nome2);
+        }
+            
+            
+        
+            
+        
 
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
